78b7e496063fc4c80f948c9a51e7b2f697ce43e3
dreamerwu <haowbdreamer@gmail.com>
2018-01-15 19:23:04 -0500

    Update differentially expression genes and pathway annotation.R

--- differentially expression genes and pathway annotation.R
+++ differentially expression genes and pathway annotation.R
@@ -70,27 +70,11 @@ venn.plot=draw.pairwise.venn(
   lty="solid",
   fill=c("red","green"),
   rotation.degree=180
-  
-  
 )
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+################################
 library("pheatmap")
 data=read.delim("D:/demo/song/mRNA/pathway_annotation_up.txt",head=T,sep="\t")
 data2=data[,2:ncol(data)]
@@ -102,6 +86,74 @@ data=read.delim("D:/demo/song/mRNA/pathway_annotation_down.txt",head=T,sep="\t")
 data2=data[,2:ncol(data)]
 row.names(data2)=data[,1:1]
 pheatmap(data2,border_size=1,border_color="black",show_rownames=FALSE,show_colnames=FALSE,legend=FALSE,fontsize_col=12,colorRampPalette(c("white","blue"))(50),cellwidth=30,cellheight=10,cluster_rows=FALSE,cluster_cols=FALSE)
+
+
+##############
+library("ggplot2")
+data=read.delim("D:/demo/song/figure/realtime.txt",head=T,sep="\t")
+
+p=ggplot(data,aes(x=cal_27,y=scc_9,color=Gene))
+pp=p+geom_point(size=5,aes(shape=group))+theme_bw()
+pp+scale_color_manual(values=c("yellow","green","blue","orangered","black","maroon2","orange","gray","darkred","lightpink1",
+"cyan4","darkolivegreen2","darkgreen","darkkhaki","darkgray","cyan2","cornsilk2","bisque","chocolate2","chartreuse2",
+"cadetblue2","burlywood2","brown1","deepskyblue4","gold","aquamarine2","antiquewhite2","darkslategray2","darkseagreen2","darkorchid2",
+"turquoise2","hotpink","thistle2","tan2","steelblue2","springgreen2","slategray2","slateblue2"))
+
+
+##############January-16-2018
+library("ggplot2")
+ls(data)
+
+
+data=read.delim("D:/demo/song/miRNA_mitochondrial/fold_change1.txt",head=T,sep="\t")
+result=ggplot(data,aes(x=resistant,y=sensitive,color=group))
+result2=result+geom_point(size=3)+scale_color_manual(values=c("green","gray","red"))+theme_bw()+ylim(c(-10,10))+xlim(c(-10,10))
+result2+geom_abline(intercept=0,slope=1,size=1,color="blue")+theme(legend.position="none")
+
+
+
+data=read.delim("D:/demo/song/miRNA_mitochondrial/fold_change2.txt",head=T,sep="\t")
+result=ggplot(data,aes(x=total,y=mitochondrial,color=group))
+result2=result+geom_point(size=3)+scale_color_manual(values=c("green","gray","red"))+theme_bw()+ylim(c(-8,8))+xlim(c(-8,8))
+result2+geom_abline(intercept=0,slope=1,size=1,color="blue")+theme(legend.position="none")
+
+
+
+
+
+data=read.delim("D:/demo/song/miRNA_mitochondrial/fold_change3.txt",head=T,sep="\t")
+result=ggplot(data,aes(x=total,y=cytoplasma,color=group))
+result2=result+geom_point(size=3)+scale_color_manual(values=c("green","gray","red"))+theme_bw()+ylim(c(-8,8))+xlim(c(-8,8))
+result3=result2+geom_abline(intercept=0,slope=1,size=1,color="blue")+theme(legend.position="none")
+result3
+#result3+geom_hline(yintercept=0.585)+geom_hline(yintercept=-0.585)+geom_vline(xintercept=0.585)+geom_vline(xintercept=-0.585)
+
+
+###Panle D    4 x 7.5
+data=read.delim("D:/demo/song/miRNA_mitochondrial/panel_D.txt",head=T,sep="\t")
+result=ggplot(data,aes(x=total_res_vs_sen,y=y))
+result2=result+geom_jitter(aes(color=group_gene,size=group),height=0.1)+scale_size_manual(values=c(2,5))+scale_color_manual(values=c("orange","yellow","blue","darkblue","pink","lightblue","brown","purple","pink4","green","red","gray","black"))+theme_bw()
+result2+xlim(c(-6.5,6.5))+geom_vline(xintercept=0.585,color="red",lty="dotted",lwd=2)+geom_vline(xintercept=-0.585,color="red",lty="dotted",lwd=2)+theme(legend.position="none")
+
+
+###panel E 
+data=read.delim("D:/demo/song/miRNA_mitochondrial/panel_E.txt",head=T,sep="\t")
+result=ggplot(data,aes(x=total_res_vs_sen,y=y))
+result2=result+geom_jitter(aes(color=color,size=size),height=0.1)+scale_size_manual(values=c(5,2))+scale_color_manual(values=c("steelblue2",
+                                                                                                                               "yellow","green","blue","orangered","tan2","maroon2","orange","slategray2","darkred","lightpink1",
+                                                                                                                               "cyan4","darkolivegreen2","darkgreen","darkkhaki","darkgray","cyan2","cornsilk2","bisque","chocolate2","chartreuse2",
+                                                                                                                               "cadetblue2","burlywood2","brown1","deepskyblue4","gold","aquamarine2","antiquewhite2","darkslategray2","darkseagreen2","darkorchid2",
+                                                                                                                               "turquoise2","hotpink","thistle2","gray","black"))                                                                                                                              
+                                                                                                                               
+                                                                                                                               
+                                                                                                                               
+                                                                                                                             #  "yellow","blue","darkblue","pink","lightblue","brown","purple","pink4","green","red","gray","black"))
+result2+xlim(c(-6.5,6.5))+geom_vline(xintercept=0.585,color="red",lty="dotted",lwd=2)+geom_vline(xintercept=-0.585,color="red",lty="dotted",lwd=2)+theme(legend.position="none")+theme_bw()
+
+
+
+
+
 
 
 
